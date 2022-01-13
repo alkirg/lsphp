fab9c7d81410c644124c9795ffcff75dbed19fb9
kav <kav@gde.ru>
2022-01-13 22:36:46 +0300

    task 1.3 const fix

--- task1/task3.php
+++ task1/task3.php
@@ -1,11 +1,14 @@
 <?php
+const MAX_YOUTH_AGE = 17;
+const MIN_ADULT_AGE = 18;
+const MIN_PENSION_AGE = 65;
 $age = 34;
 
-if ($age >= 1 && $age <= 17) {
+if ($age >= 1 && $age <= MAX_YOUTH_AGE) {
     echo 'Вам ещё рано работать';
-} elseif ($age >= 18 && $age <= 65) {
+} elseif ($age >= MIN_ADULT_AGE && $age <= MIN_PENSION_AGE) {
     echo 'Вам еще работать и работать';
-} elseif ($age > 65) {
+} elseif ($age > MIN_PENSION_AGE) {
     echo 'Вам пора на пенсию';
 } else {
     echo 'Неизвестный возраст';
